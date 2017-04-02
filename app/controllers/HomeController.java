bf1bc9e6b86cdda0f71e64437a2ca4f5eec01270
Geetika gupta <geetika.gupta@knoldus.in>
2017-04-02 15:39:08 +0530

    1. Added view template and related controller. 2. Added class for table details. 3. WIP for the same.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,23 +1,41 @@
 package controllers;
 
-import play.mvc.*;
-
-import views.html.*;
+import com.google.inject.Inject;
+import models.TableDetails;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import services.PrestoService;
+import views.html.home;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+  Form<TableDetails> formData = Form.form(TableDetails.class);
+  @Inject() PrestoService prestoService;
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
+  /**
+   * An action that renders an HTML page with a welcome message.
+   * The configuration in the <code>routes</code> file means that
+   * this method will be called when the application receives a
+   * <code>GET</code> request with a path of <code>/</code>.
+   */
+  public Result index() {
+    return ok(home.render(formData));
+  }
+
+  public Result getData() {
+    Form<TableDetails> formData = Form.form(TableDetails.class).bindFromRequest();
+    if (formData.hasErrors()) {
+      return badRequest();
+    } else {
+      TableDetails tableDetails = formData.get();
+      String result =
+          prestoService.getData(tableDetails.getDatabaseName(), tableDetails.getTableName());
+      return ok(result);
     }
-
+  }
 }
+
--- app/services/PrestoService.java
+++ app/services/PrestoService.java
@@ -1,74 +1,75 @@
 package services;
 
-
-import scala.Int;
-
-import java.sql.*;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PrestoService {
-    public static void main(String args[]) {
+  public String getData(String databaseName, String tableName) {
 
-        final String JDBC_DRIVER = "com.facebook.presto.jdbc.PrestoDriver";
-        final String DB_URL = "jdbc:presto://localhost:8086/carbondata/demo";
+    final String JDBC_DRIVER = "com.facebook.presto.jdbc.PrestoDriver";
+    final String DB_URL = "jdbc:presto://localhost:8086/hive-hadoop2/" + databaseName;
+    String result = "";
 
-        //  Database Credentials
-        final String USER = "username";
-        final String PASS = "password";
+    //  Database Credentials
+    final String USER = "username";
+    final String PASS = "password";
 
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            //STEP 2: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
+    Connection conn = null;
+    Statement stmt = null;
+    try {
+      //STEP 2: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
 
-            //STEP 3: Open a connection
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            //conn.setCatalog("hive");
+      //STEP 3: Open a connection
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-            //STEP 4: Execute a query
-            stmt = conn.createStatement();
-            String sql;
-            sql = "select * from employee";
+      //STEP 4: Execute a query
+      stmt = conn.createStatement();
+      String sql;
+      sql = "select * from " + tableName;
 
-            ResultSet result=stmt.executeQuery(sql);
-          /*  ResultSet res = stmt.executeQuery(sql);*/
+      ResultSet res = stmt.executeQuery(sql);
 
-            /*System.out.println(">>>>"+res);*/
-            //STEP 5: Extract data from result set
-           /* while (res.next()) {
+      //STEP 5: Extract data from result set
+      result = "name" + "age\n";
+            while (res.next()) {
                 //Retrieve by column name
 
-               *//* String imei = res.getString("name");
-                Integer check_year = res.getInt("age");
+                String name = res.getString("name");
+                Integer age = res.getInt("age");
 
                 //Display values
-                System.out.println("imei: " + imei + "\n check_year : " + check_year);*//*
+                System.out.println("imei: " + name + "\n check_year : " + age);
                 System.out.println("success");
-            }*/
-            //STEP 6: Clean-up environment
-            // rs.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException se) {
-            //Handle errors for JDBC
-            se.printStackTrace();
-        } catch (Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null) stmt.close();
-            } catch (SQLException se2) {
+                result += name + age + "\n";
             }
-            try {
-                if (conn != null) conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
+      //STEP 6: Clean-up environment
+       res.close();
+      stmt.close();
+      conn.close();
+    } catch (SQLException se) {
+      //Handle errors for JDBC
+      se.printStackTrace();
+    } catch (Exception e) {
+      //Handle errors for Class.forName
+      e.printStackTrace();
+    } finally {
+      //finally block used to close resources
+      try {
+        if (stmt != null) stmt.close();
+      } catch (SQLException se2) {
+      }
+      try {
+        if (conn != null) conn.close();
+      } catch (SQLException se) {
+        se.printStackTrace();
+      }
     }
+    return result.toString();
+  }
 }
 
 
